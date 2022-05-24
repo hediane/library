a45e450615198fc8218566108de626a9d7abefed
hediane <49047327+hediane@users.noreply.github.com>
2022-05-24 15:40:50 +0200

    shared library

--- vars/pipeline_library.groovy
+++ vars/pipeline_library.groovy
@@ -27,6 +27,8 @@ def call (Map config)
                  sh "ls -la ${pwd()}"
                  sh "ls -la ${pwd()}/source"
                  sh "ls -la ${pwd()}/source/DevOpsProject/"
+                 sh "cd /var/jenkins_home/workspace/"
+                 sh "mkdir aoso"
                  sh "cp -r ${pwd()}/source/DevOpsProject /var/jenkins_home/workspace/aoso"
                  sh "ls -la /var/jenkins_home/workspace/aoso "
             }
