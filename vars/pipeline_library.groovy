59a470dcf41bd271b2977a0796360192b176cca0
hediane <49047327+hediane@users.noreply.github.com>
2022-05-25 11:59:44 +0200

    destinationfile

--- vars/pipeline_library.groovy
+++ vars/pipeline_library.groovy
@@ -29,8 +29,8 @@ def call (Map config)
                  sh "ls -la ${pwd()}/source/DevOpsProject/"
                  sh "cd /var/jenkins_home/workspace/"
                  //sh "mkdir aoso | mkdir /aoso/DevOps | mkdir /aoso/DevOps/back"
-                 sh "rm -rf ${config.DestinationFile}"
-                 ///sh "cp -r ${pwd()}/source/DevOpsProject ${config.DestinationFile}"
+                 //sh "rm -rf ${config.DestinationFile}"
+                 sh "cp -r ${pwd()}/source/DevOpsProject ${config.DestinationFile}"
                  sh "ls -la ${config.DestinationFile} "
             }
             stage('location of docker-compose') 
