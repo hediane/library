d92e8c253af71ff8f3730f0af2721688193945ad
hediane <49047327+hediane@users.noreply.github.com>
2022-05-26 13:56:26 +0200

    shared library

--- vars/library_front.groovy
+++ vars/library_front.groovy
@@ -40,8 +40,8 @@ def call (Map config)
                  sh "cp -r ${pwd()}/scripts/Front/Dockerfile ${config.DestinationFileFront}"
                  sh "cp -r ${pwd()}/scripts/Front/docker-compose-front.yml ${config.DestinationFileFront}"
                  sh "cp -r ${pwd()}/scripts/Front/nginx ${config.DestinationFileFront}"
-                  sh "cp -r ${pwd()}/source/DevOpsFront/package.json ${config.DestinationFile}"
-                 sh "cp -r ${pwd()}/source/DevOpsFront/package-lock.json ${config.DestinationFile}"
+                  sh "cp -r ${pwd()}/source/DevOpsFront/package.json ${config.DestinationFileFront}"
+                 sh "cp -r ${pwd()}/source/DevOpsFront/package-lock.json ${config.DestinationFileFront}"
                  sh "ls -la ${config.DestinationFileFront} "
             }
             stage('location of docker-compose') 
