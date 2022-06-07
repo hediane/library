adab5515536817211966f6ec605b6e66923adae6
hediane <49047327+hediane@users.noreply.github.com>
2022-06-07 16:45:00 +0200

    shared library

--- vars/library_back.groovy
+++ vars/library_back.groovy
@@ -30,17 +30,15 @@ def call (Map config)
                 }*/
             stage('SonarQube Analysis') 
                 {  
-                    
                     withSonarQubeEnv() {
                     //sh "dotnet restore source/DevOpsProject/DevOpsProject/DevOpsProject.csproj"
                     dir("source/${config.ProjectName}") {
                     sh " ls -la ${pwd()}"
                     def scannerHome = tool name: 'sonarscanner';
                     //sh "dotnet ${scannerHome}/bin/sonar-scanner  /k:Aoso /d:sonar.host.url= http://192.168.56.113:9000"
-                    sh "${scannerHome}/bin/sonar-scanner begin \ -Dsonar.projectKey=Aoso \ -Dsonar.sources=. \ -Dsonar.host.url=http://192.168.56.113:9000 \ -Dsonar.login=ab9f339761ec69b84c33072c739b28b604d3f8ce "
+                    sh "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=Aoso  -Dsonar.sources=. -Dsonar.host.url=http://192.168.56.113:9000  -Dsonar.login=ab9f339761ec69b84c33072c739b28b604d3f8ce "
                     //sh "${scannerHome}/bin/sonar-scanner -D /k:Aoso -D /d:sonar.host.url=http://192.168.56.113:9000  -D /d:sonar.login=ab9f339761ec69b84c33072c739b28b604d3f8ce "
                     sh "dotnet build DevOpsProject.csproj"
-                    sh "${scannerHome}/bin/sonar-scanner end"
                     }
                 }
                 }
