af58b996c4e78ac17280e2449e170b1c39b32846
hediane <49047327+hediane@users.noreply.github.com>
2022-06-13 13:55:45 +0200

    test

--- vars/library_back.groovy
+++ vars/library_back.groovy
@@ -115,12 +115,12 @@ def call (Map config)
                 {   def scannerHome = tool name: 'sonarscanner';
                     withSonarQubeEnv('sonarQube') {
                     //sh "dotnet restore source/DevOpsProject/DevOpsProject/DevOpsProject.csproj"
-                            dir("${config.FrontPath}") {
+                            dir("${config.ProjectName}") {
                                     sh " ls -la ${pwd()}"
                                     //sh "dotnet ${scannerHome}/bin/sonar-scanner  /k:AosoFront /d:sonar.host.url= http://192.168.56.113:9000"
                                     sh "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=AosoBack -Dsonar.login=a650a854dfc5fdfd835f432b6cbf52f369f6a2b1 -Dsonar.scm.provider=git "
                                     //sh "${scannerHome}/bin/sonar-scanner -D /k:Aoso -D /d:sonar.host.url=http://192.168.56.113:9000  -D /d:sonar.login=ab9f339761ec69b84c33072c739b28b604d3f8ce "
-                                    sh "dotnet build"
+                                    sh "dotnet build "
                                     //sh "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=Aosofront "
                                     
                             }
