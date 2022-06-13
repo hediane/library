f485e625c4a7b339a7d9420778711b6cc453e74b
hediane <49047327+hediane@users.noreply.github.com>
2022-06-13 12:37:33 +0200

    test

--- vars/library_back.groovy
+++ vars/library_back.groovy
@@ -103,7 +103,7 @@ def call (Map config)
                     def scannerHome = tool name:'SonarScanner for MSBuild'
                     withSonarQubeEnv('sonarQube')
                     {
-                        dir("/source/${config.ProjectName}") {
+                        dir("source/${config.ProjectName}") {
                         sh " ls -la ${pwd()}"
                         sh "dotnet ${scannerHome}/SonarScanner.MSBuild.dll begin /k:Aoso /d:sonar.login=a650a854dfc5fdfd835f432b6cbf52f369f6a2b1 "
                         sh "dotnet build DevopsProject.csproj"
