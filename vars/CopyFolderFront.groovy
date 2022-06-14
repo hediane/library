2de23e7be3575fe55d99e0261196fae36c2d6bbe
hediane <49047327+hediane@users.noreply.github.com>
2022-06-14 23:50:24 +0200

    new

---
+++ vars/CopyFolderFront.groovy
@@ -0,0 +1,31 @@
+def call (Map config)
+{
+    node
+        { 
+               stage ('copy all file from BACK')
+            {    
+                 sh "ls -la ${pwd()}"
+                 sh "ls -la ${pwd()}/source"
+        
+                 //sh "cd /var/jenkins_home/workspace/"
+                 sh "mkdir -p /srv/aoso/DevOps/backend "
+                 //sh "rm -rf /var/jenkins_home/workspace/aoso/DevOps/back"
+                sh "ls -a ${config.DestinationFolder1}"
+                sh "ls -a ${config.DestinationFolder2}"
+                sh "ls -a ${config.DestinationFile}"
+                 //sh "rm -rf ${config.DestinationFile}"
+                 //sh "cp -r ${pwd()}/source/DevOpsProject ${config.DestinationFile}"
+                 //sh "rm -rf ${config.DestinationFile}/Dockerfile"
+                 sh "cp -r ${config.DestinationFolder} ${config.DestinationFile}"
+                 sh "cp -r ${pwd()}${config.DockerfileLocation} ${config.DestinationProject}"
+                 sh "cp -r ${pwd()}${config.dockerComposeFileLocation} ${config.DestinationFile}"
+                 sh "cp -r ${pwd()}${config.nginxLocation} ${config.DestinationFile}"
+                 //sh "cp -r ${pwd()}${config.elasticsearch} ${config.DestinationFile}"
+                 sh "ls -la ${config.DestinationNginx} "
+                 sh "ls -la ${config.DestinationFile} "
+            }
+           
+
+        }
+           
+}
