56cfe182029490b692ee8e8f9a75d50e07dfc2f7
hediane <49047327+hediane@users.noreply.github.com>
2022-06-16 10:45:56 +0200

    updtae

--- vars/SonarQubeDotnet.groovy
+++ vars/SonarQubeDotnet.groovy
@@ -3,24 +3,24 @@ def call (Map config)
     node
     {
          
-        stage('SONAR BUILD') 
+        /*stage('SONAR BUILD') 
                 {   sh "${config.dockerComposeLocation} -f ${config.dockerComposeSonarQube} build"
-                    sh "${config.dockerComposeLocation} -f ${config.dockerComposeSonarQube} up -d --scale sonarqube=3"
-                }
+                    sh "${config.dockerComposeLocation} -f ${config.dockerComposeSonarQube} up -d"
+                }*/
         
-        /*stage('Sonar Quality Analysis')
+        stage('Sonar Quality Analysis')
                {
                     def scannerHome = tool name:'SonarScanner for MSBuild'
                     withSonarQubeEnv('SonarQube')
                     {
                         dir("${config.ProjectName}") {
                         sh " ls -la ${pwd()}"
-                        sh "dotnet ${scannerHome}/SonarScanner.MSBuild.dll begin /k:aosohediane /d:sonar.login=23b5d4c1c1f76c539f1d0019945228a4003d6a51 "
+                        sh "dotnet ${scannerHome}/SonarScanner.MSBuild.dll begin /k:Back /d:sonar.login=aosora "
                         sh "dotnet build"
-                        sh "dotnet ${scannerHome}/SonarScanner.MSBuild.dll end /d:sonar.login=23b5d4c1c1f76c539f1d0019945228a4003d6a51"
+                        sh "dotnet ${scannerHome}/SonarScanner.MSBuild.dll end /d:sonar.login=aosora"
                         //a650a854dfc5fdfd835f432b6cbf52f369f6a2b1"
                         }
                     }
-                }*/
+                }
     }
 }
