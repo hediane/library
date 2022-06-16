2f4db78def73e330ccb83a2faea44aacff1d92be
hediane <49047327+hediane@users.noreply.github.com>
2022-06-16 16:06:51 +0200

    up

--- vars/SonarQubeAngular.groovy
+++ vars/SonarQubeAngular.groovy
@@ -2,14 +2,14 @@ def call (Map config)
 {
     node
     {
-       stage('Quality Gate') 
+       stage('Analysis quality') 
             {   def scannerHome = tool name: 'sonarscanner';
                     withSonarQubeEnv('SonarQube') {
                     //sh "dotnet restore source/DevOpsProject/DevOpsProject/DevOpsProject.csproj"
                             dir("${config.FrontPath}") {
                                     sh " ls -la ${pwd()}"
                                     //sh "dotnet ${scannerHome}/bin/sonar-scanner  /k:AosoFront /d:sonar.host.url= http://192.168.56.113:9000"
-                                    sh "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=Front -Dsonar.login=eb77d83ff776ed7e511ccb6f98db5163f1af4f9f -Dsonar.scm.provider=git "
+                                    sh "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=Front -Dsonar.login=eb77d83ff776ed7e511ccb6f98db5163f1af4f9f "
                                     //sh "${scannerHome}/bin/sonar-scanner -D /k:Aoso -D /d:sonar.host.url=http://192.168.56.113:9000  -D /d:sonar.login=ab9f339761ec69b84c33072c739b28b604d3f8ce "
                                     //sh "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=Aosofront "
                                     
