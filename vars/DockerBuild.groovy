3b771d1958028b9b5b6326aa0344cb6b4f4f9fa6
hediane <49047327+hediane@users.noreply.github.com>
2022-06-21 13:46:38 +0200

    up

--- vars/DockerBuild.groovy
+++ vars/DockerBuild.groovy
@@ -2,9 +2,26 @@ def call (Map config)
 {
     node
         { 
-           stage('Docker BUILD') 
-                {  //sh "${config.dockerComposeLocation} -f ${config.dockerComposeDestination} build"
-                    sh "${config.dockerComposeLocation} -f ${config.dockerComposeDestination} up -d"
+            stage('DEPLOY APP ') 
+                {  
+                    wrap([$class: 'BuildUser']) {
+                    def user = env.BUILD_USER_ID
+                    echo "${user}"
+                    //def list = ['AmaniGHADDAB']
+                    echo "${config.devValidator}"
+                    if ("${config.devValidator}".contains("${user}"))
+                    {
+                       sh "${config.dockerComposeLocation} -f ${config.dockerComposeDestination} up -d"
+                    }
+                    else
+                    {
+                           
+                        //slackSend color: 'danger', channel: '#devops', message: "<${currentBuild.absoluteUrl}|Server build ${env.BUILD_NUMBER}> failed to deploy build "
+	                       echo"Don't have access"
+                    }
+                 }
                 }
+
         }
+           
 }
