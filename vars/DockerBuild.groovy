8733df8e431cc7a4d605bb582abdd34514fa326e
hediane <49047327+hediane@users.noreply.github.com>
2022-06-21 15:25:32 +0200

    up

--- vars/DockerBuild.groovy
+++ vars/DockerBuild.groovy
@@ -6,9 +6,10 @@ def call (Map config)
                 {  
                     wrap([$class: 'BuildUser']) {
                     def user = env.BUILD_USER_ID
-                    echo "${user}"
+                    //echo "${user}"
                     //def list = ['AmaniGHADDAB']
-                    echo "${config.devValidator}"
+                    //echo "${config.devValidator}"
+                
                     if ("${config.devValidator}".contains("${user}"))
                     {
                        sh "${config.dockerComposeLocation} -f ${config.dockerComposeDestination} up -d"
@@ -17,11 +18,17 @@ def call (Map config)
                     {    
                         //slackSend color: 'danger', channel: '#devops', message: "<${currentBuild.absoluteUrl}|Server build ${env.BUILD_NUMBER}> failed to deploy build "
 	                       
-                            input message: 'Approve Deploy?', ok: 'Yes'
-                             
+                            //input message: 'Do you want to approve the deploy in production?', ok: 'Yes'
+                            isApproved = input(id: 'someId',message: 'Approve?', submitter: 'someuser',
+                            parameters: [choice( 
+                                        choices: ['No', 'Yes'],
+                                        description: 'some description',
+                                        name: 'some name')]
+                                ) == 'Yes'
+                                            
                     }
-                 }
-                }
+                                }
+                                }
 
         }
            
