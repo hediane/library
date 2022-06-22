b1b32ac99af6f6c6fd92f77f6c2adbb68795caa5
hediane <49047327+hediane@users.noreply.github.com>
2022-06-22 17:27:11 +0200

    up

--- vars/NexusFront.groovy
+++ vars/NexusFront.groovy
@@ -2,7 +2,7 @@ def call (Map config)
 {
     node
         {
-            //dir("/var/jenkins_home/workspace/Front_main/source/DevOpsFront") {
+            dir("/var/jenkins_home/workspace/Front_main/source/DevOpsFront") {
                         stage('Packing') 
 
                         {
@@ -13,7 +13,7 @@ def call (Map config)
                             sh "ls -a ${config.dist}"
 
                         }
-            //}
+            }
             /*stage('Publish') 
                 { 
                  sh "dotnet nuget push /var/jenkins_home/workspace/Back_main/source/DevOpsProject/bin/Debug/aosora.1.0.0.nupkg -s http://87.106.205.95:4002/repository/nuget-hosted/ -k 5f15c27d-1f8a-3baa-a136-cd624ba7c9b7 "
