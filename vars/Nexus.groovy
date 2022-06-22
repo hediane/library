79902e166a92c38f2c69f5ef9e0591fc4203c7fb
hediane <49047327+hediane@users.noreply.github.com>
2022-06-22 11:25:50 +0200

    up

--- vars/Nexus.groovy
+++ vars/Nexus.groovy
@@ -11,7 +11,6 @@ def call (Map config)
                     sh "ls -a /var/jenkins_home/workspace/Back_main/source/DevOpsProject/bin/Debug/"
                     //sh"dotnet nuget push /srv/aoso/DevOps/backend/DevOpsProject/bin/Debug/aosora.1.0.0.nupkg --Source http://192.168.56.115:8081/repository/nuget-hosted/ -ApiKey 5f15c27d-1f8a-3baa-a136-cd624ba7c9b7 -source ./repository/Backend/"
                     //sh 'dotnet pack '
-                
                 }
             stage('Publish') 
                 {  //sh "${config.dockerComposeLocation} -f ${config.dockerComposeElasticDestintination} build "
@@ -19,8 +18,9 @@ def call (Map config)
                     //echo "Buid Image with docker-compose"
                     //echo "${config.dockerfileLocation}",
                     //sh "dotnet pack ${config.ProjectName}"
-                    //sh"dotnet nuget push aosora.1.0.0.nupkg --Source http://192.168.56.115:8081/repository/nuget-hosted/ -ApiKey 5f15c27d-1f8a-3baa-a136-cd624ba7c9b7 -source ./repository/Backend/"
+                    sh "dotnet nuget push *.nupkg --Source http://192.168.56.115:8081/repository/nuget-hosted/ -ApiKey 5f15c27d-1f8a-3baa-a136-cd624ba7c9b7 "
                     //sh 'dotnet pack '
+                    
                 
                 }
         }
