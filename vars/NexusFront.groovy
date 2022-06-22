21ee1fc64b248cd542de79229c8c04ebdff5691e
hediane <49047327+hediane@users.noreply.github.com>
2022-06-22 17:43:37 +0200

    up

--- vars/NexusFront.groovy
+++ vars/NexusFront.groovy
@@ -13,14 +13,16 @@ def call (Map config)
                             sh "ls -a ${config.dist}"
 
                         }
-            }
-            /*stage('Publish') 
+            
+            stage('Publish') 
                 { 
-                 sh "dotnet nuget push /var/jenkins_home/workspace/Back_main/source/DevOpsProject/bin/Debug/aosora.1.0.0.nupkg -s http://87.106.205.95:4002/repository/nuget-hosted/ -k 5f15c27d-1f8a-3baa-a136-cd624ba7c9b7 "
-                //sh 'dotnet nuget push **\\nupkgs\\*.nupkg -s http://192.168.56.115:8081/repository/nuget-hosted/ -k 5f15c27d-1f8a-3baa-a136-cd624ba7c9b7 '
+                sh 'npm login --registry http://192.168.56.115:8081/repository/aosoFront/'
+               // sh 'ng build ng-trc-lib'
+                sh 'npm publish /var/jenkins_home/workspace/Front_main/source/DevOpsFront/dev-ops-front-0.0.0.tgz'
                     
                 
-                }*/
+                }
+        }
         }
         
 }
