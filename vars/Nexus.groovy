560ba29c4b05bd1fda9b448c46a6148e2aa34e19
hediane <49047327+hediane@users.noreply.github.com>
2022-06-22 13:41:14 +0200

    up

--- vars/Nexus.groovy
+++ vars/Nexus.groovy
@@ -18,7 +18,7 @@ def call (Map config)
                     //echo "Buid Image with docker-compose"
                     //echo "${config.dockerfileLocation}",
                     //sh "dotnet pack ${config.ProjectName}"
-                    sh "dotnet push /var/jenkins_home/workspace/Back_main/source/DevOpsProject/bin/Debug/aosora.1.0.0.nupkg -s http://192.168.56.115:8081/repository/nuget-hosted/ -k 5f15c27d-1f8a-3baa-a136-cd624ba7c9b7 "
+                    sh "dotnet nuget push /var/jenkins_home/workspace/Back_main/source/DevOpsProject/bin/Debug/aosora.1.0.0.nupkg -s http://192.168.56.115:8081/repository/nuget-hosted/ -k 5f15c27d-1f8a-3baa-a136-cd624ba7c9b7 "
                     //sh 'dotnet pack '
                     
                 
