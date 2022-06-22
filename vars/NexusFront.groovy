83858a614f97080f9fd93a6bff94dc560e862d10
hediane <49047327+hediane@users.noreply.github.com>
2022-06-22 17:12:06 +0200

    up

--- vars/NexusFront.groovy
+++ vars/NexusFront.groovy
@@ -9,6 +9,7 @@ def call (Map config)
                             
                             sh "cd ${config.dist}"
                             sh 'npm pack'
+                             sh 'ls -a'
                             sh "ls -a ${config.dist}"
 
                         }
