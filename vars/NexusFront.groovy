221b1d8712da40cfeac95b41c20ca5cc85b444c1
hediane <49047327+hediane@users.noreply.github.com>
2022-06-23 11:31:31 +0200

    up

--- vars/NexusFront.groovy
+++ vars/NexusFront.groovy
@@ -19,6 +19,8 @@ def call (Map config)
                 //sh 'npm login --registry http://192.168.56.115:8081/repository/aosoFront/'
                // sh 'ng build ng-trc-lib'
                 //sh 'npm publish /var/jenkins_home/workspace/Front_main/source/DevOpsFront/dev-ops-front-0.0.0.tgz'
+                   
+                   sh 'npm install --no-package-lock'
                     sh "npm adduser --registry=http://192.168.56.115:8081/repository/aosoFront/ --always-auth"
 
                     sh "npm login --registry=http://192.168.56.115:8081/repository/aosoFront/ publish /var/jenkins_home/workspace/Front_main/source/DevOpsFront/dev-ops-front-0.0.0.tgz"
