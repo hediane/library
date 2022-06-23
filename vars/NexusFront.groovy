a246e3257f16d4f2c982d5b13af9635b56f6032c
hediane <49047327+hediane@users.noreply.github.com>
2022-06-23 11:12:16 +0200

    up

--- vars/NexusFront.groovy
+++ vars/NexusFront.groovy
@@ -21,7 +21,7 @@ def call (Map config)
                 //sh 'npm publish /var/jenkins_home/workspace/Front_main/source/DevOpsFront/dev-ops-front-0.0.0.tgz'
                     sh "npm adduser --registry=http://192.168.56.115:8081/repository/aosoFront/ --always-auth"
 
-                    sh "npm --registry=http://192.168.56.115:8081/repository/aosoFront/ publish /var/jenkins_home/workspace/Front_main/source/DevOpsFront/dev-ops-front-0.0.0.tgz"
+                    sh "npm login --registry=http://192.168.56.115:8081/repository/aosoFront/ publish /var/jenkins_home/workspace/Front_main/source/DevOpsFront/dev-ops-front-0.0.0.tgz"
                 
                 }
         }
