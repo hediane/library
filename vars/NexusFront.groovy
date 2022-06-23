8c828c3fa17a7ad0058449658dde8db8aed721dd
hediane <49047327+hediane@users.noreply.github.com>
2022-06-23 13:07:15 +0200

    up

--- vars/NexusFront.groovy
+++ vars/NexusFront.groovy
@@ -3,10 +3,7 @@ def call (Map config)
     
     node
         {
-            env
-                    {
-                        NPM_TOKEN: ${secrets.NPM_TOKEN}
-                    }
+            
             dir("/var/jenkins_home/workspace/Front_main/source/DevOpsFront") {
                         stage('Packing') 
 
@@ -24,12 +21,11 @@ def call (Map config)
                 //sh 'npm login --registry http://192.168.56.115:8081/repository/aosoFront/'
                // sh 'ng build ng-trc-lib'
                 //sh 'npm publish /var/jenkins_home/workspace/Front_main/source/DevOpsFront/dev-ops-front-0.0.0.tgz'
-                   sh "npm token create"   
+                   //sh "npm token create"   
                        
-                   sh 'npm install --no-package-lock'
-                   sh "npm adduser --registry=http://192.168.56.115:8081/repository/aosoFront/ --always-auth"
-
-                    sh "npm login --registry=http://192.168.56.115:8081/repository/aosoFront/ publish /var/jenkins_home/workspace/Front_main/source/DevOpsFront/dev-ops-front-0.0.0.tgz"
+                   //sh 'npm install --no-package-lock'
+                   //sh "npm adduser --registry=http://192.168.56.115:8081/repository/aosoFront/ --always-auth"
+                  //sh "npm login --registry=http://192.168.56.115:8081/repository/aosoFront/ publish /var/jenkins_home/workspace/Front_main/source/DevOpsFront/dev-ops-front-0.0.0.tgz"
                 
                 }
         }
