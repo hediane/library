1bfaade6542868df77601d67190818c2a67eec53
hediane <49047327+hediane@users.noreply.github.com>
2022-06-23 10:38:42 +0200

    up

--- vars/NexusFront.groovy
+++ vars/NexusFront.groovy
@@ -16,10 +16,10 @@ def call (Map config)
             
             stage('Publish') 
                 { 
-                sh 'npm login --registry http://192.168.56.115:8081/repository/aosoFront/'
+                //sh 'npm login --registry http://192.168.56.115:8081/repository/aosoFront/'
                // sh 'ng build ng-trc-lib'
-                sh 'npm publish /var/jenkins_home/workspace/Front_main/source/DevOpsFront/dev-ops-front-0.0.0.tgz'
-                    
+                //sh 'npm publish /var/jenkins_home/workspace/Front_main/source/DevOpsFront/dev-ops-front-0.0.0.tgz'
+                    sh 'npm --registry=http://192.168.56.115:8081/repository/aosoFront/ publish /var/jenkins_home/workspace/Front_main/source/DevOpsFront/dev-ops-front-0.0.0.tgz'
                 
                 }
         }
