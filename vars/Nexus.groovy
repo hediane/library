3db1333efda872d9765bbffa4ffc4b8dd54bfaeb
hediane <49047327+hediane@users.noreply.github.com>
2022-06-24 14:26:43 +0200

    up

--- vars/Nexus.groovy
+++ vars/Nexus.groovy
@@ -5,9 +5,10 @@ def call (Map config)
     
                 stage('Packing') 
                 {  
-                    //sh "${config.dockerComposeLocation} -f ${config.dockerComposeNexusDestination} up -d"
+                   
+                   //sh "${config.dockerComposeLocation} -f ${config.dockerComposeNexusDestination} up -d"
                    sh "dotnet pack ${config.ProjectName}"
-                   sh "ls -a /var/jenkins_home/workspace/Back_main/source/DevOpsProject/bin/Debug/"
+                   sh "ls -a /var/lib/jenkins/workspace/Back_main/source/DevOpsProject/bin/Debug/"
                     //sh 'dotnet clean'
                     //sh 'dotnet build --configuration Release'
                     //sh 'dotnet pack --no-build --output nupkgs'
@@ -15,7 +16,7 @@ def call (Map config)
                 }
             stage('Publish') 
                 { 
-                 sh "dotnet nuget push /var/jenkins_home/workspace/Back_main/source/DevOpsProject/bin/Debug/aosora.1.0.0.nupkg -s http://87.106.205.95:4002/repository/nuget-hosted/ -k 5f15c27d-1f8a-3baa-a136-cd624ba7c9b7 "
+                 sh "dotnet nuget push /var/lib/jenkins/workspace/Back_main/source/DevOpsProject/bin/Debug/bin/Debug/aosora.1.0.0.nupkg -s http://87.106.205.95:4002/repository/nuget-hosted/ -k 5f15c27d-1f8a-3baa-a136-cd624ba7c9b7 "
                 //sh 'dotnet nuget push **\\nupkgs\\*.nupkg -s http://192.168.56.115:8081/repository/nuget-hosted/ -k 5f15c27d-1f8a-3baa-a136-cd624ba7c9b7 '
                     
                 
