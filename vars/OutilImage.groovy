afe9437f3bf8d53da9ef7b11548f439b07a0cf5e
hediane <49047327+hediane@users.noreply.github.com>
2022-06-24 16:12:50 +0200

    up

--- vars/OutilImage.groovy
+++ vars/OutilImage.groovy
@@ -2,13 +2,14 @@ def call (Map config)
 {
     node
         {
-            stage('Install') 
+            stage('Build Ansible ') 
                 {    
                    
                    // sh "${config.dockerComposeLocation} -f ${config.elasticsearch} up -d"
                     //sh "${config.dockerComposeLocation} -f ${config.dockerComposeSonarQube} up -d"
-                    sh "${config.dockerComposeLocation} -f ${config.dockerComposeNexus} up -d"
-                
+                    //sh "${config.dockerComposeLocation} -f ${config.dockerComposeNexus} up -d"
+                    sh 'docker build -t ansible-app .'
+                    sh 'docker run -dp 8888:8888 ansible-app'
                 } 
         }
 }
