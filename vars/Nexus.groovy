79c6763e40656b59cd4546893ad884f51ec8d35b
hediane <49047327+hediane@users.noreply.github.com>
2022-06-24 14:27:33 +0200

    up

--- vars/Nexus.groovy
+++ vars/Nexus.groovy
@@ -17,9 +17,7 @@ def call (Map config)
             stage('Publish') 
                 { 
                  sh "dotnet nuget push /var/lib/jenkins/workspace/Back_main/source/DevOpsProject/bin/Debug/bin/Debug/aosora.1.0.0.nupkg -s http://87.106.205.95:4002/repository/nuget-hosted/ -k 5f15c27d-1f8a-3baa-a136-cd624ba7c9b7 "
-                //sh 'dotnet nuget push **\\nupkgs\\*.nupkg -s http://192.168.56.115:8081/repository/nuget-hosted/ -k 5f15c27d-1f8a-3baa-a136-cd624ba7c9b7 '
-                    
-                
+                //sh 'dotnet nuget push **\\nupkgs\\*.nupkg -s http://192.168.56.115:8081/repository/nuget-hosted/ -k 5f15c27d-1f8a-3baa-a136-cd624ba7c9b7 '     
                 }
         }
         
