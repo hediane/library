14a0809792d377b5b8fcdd9c43d83101cd6360d6
hediane <49047327+hediane@users.noreply.github.com>
2022-06-27 15:49:24 +0200

    up

--- vars/Nexus.groovy
+++ vars/Nexus.groovy
@@ -17,7 +17,7 @@ def call (Map config)
                 }
             stage('Publish') 
                 { 
-                 sh "dotnet nuget push /bin/Debug/DevOpsProject.1.0.0.nupkg -s http://192.168.56.115:8081/repository/nuget-hosted/ -k 5f15c27d-1f8a-3baa-a136-cd624ba7c9b7 "
+                 sh "dotnet nuget push /var/lib/jenkins/workspace/Back_main/source/DevOpsProject/bin/Debug/DevOpsProject.1.0.0.nupkg -s http://192.168.56.115:8081/repository/nuget-hosted/ -k 5f15c27d-1f8a-3baa-a136-cd624ba7c9b7 "
                 //sh 'dotnet nuget push **\\nupkgs\\*.nupkg -s http://192.168.56.115:8081/repository/nuget-hosted/ -k 5f15c27d-1f8a-3baa-a136-cd624ba7c9b7 '
                     
                 
