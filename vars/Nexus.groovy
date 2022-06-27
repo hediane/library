22d93d02117abf10f61ebee369e45c7b51ca745b
hediane <49047327+hediane@users.noreply.github.com>
2022-06-27 15:33:06 +0200

    up

--- vars/Nexus.groovy
+++ vars/Nexus.groovy
@@ -2,14 +2,14 @@ def call (Map config)
 {
     node
         {
-        dir("/var/lib/jenkins/workspace/Back_main/source/DevOpsProject")
+        dir("source/DevOpsProject")
              {   stage('Packing') 
                 {  
                    
                 //sh "${config.dockerComposeLocation} -f ${config.dockerComposeNexusDestination} up -d"
                    sh 'dotnet clean'
                    sh 'dotnet build --configuration Release'
-                   sh "dotnet pack  /var/lib/jenkins/workspace/Back_main/source/DevOpsProject/bin/Release/net5.0/ --no-build --output nupkgs"
+                   sh "dotnet pack --no-build --output nupkgs"
                    //sh "ls -a /var/lib/jenkins/workspace/Back_main/source/DevOpsProject/bin/Debug/"
                    //sh "ls -a /var/lib/jenkins/workspace/Back_main/source/DevOpsProject/bin/Debug/aosora.1.0.0.nupkg"
                     //sh 'dotnet clean'
