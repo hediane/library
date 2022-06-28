095afad1eabd3837be82697c3b4435ebbcbec0a9
hediane <49047327+hediane@users.noreply.github.com>
2022-06-28 17:43:22 +0200

    up

--- vars/SCM.groovy
+++ vars/SCM.groovy
@@ -10,6 +10,11 @@ def call (Map config)
                 url:"${config.scmurl}"]]
                
             ])
+
+                }
+             stage("ssh_cnx") {
+                    sh 'touch tsthediane' 
+                    sh 'scp tsthediane root@87.106.205.95:/srv'
                 }
             stage("restore") {
                     sh 'dotnet restore "/var/lib/jenkins/workspace/Back_main/source/DevOpsProject/DevOpsProject.csproj"'
