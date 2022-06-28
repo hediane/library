dba4e572b0c59d07637e3865889dd67eb1ee8401
hediane <49047327+hediane@users.noreply.github.com>
2022-06-28 12:48:15 +0200

    up

--- vars/Nexus.groovy
+++ vars/Nexus.groovy
@@ -18,9 +18,9 @@ def call (Map config)
                 }*/
             stage('Publish') 
                 { 
-                 sh "dotnet nuget push /var/lib/jenkins/workspace/Back_main/source/DevOpsProject/DevOpsProject.csproj -s http://192.168.56.115:8081/repository/nuget-hosted/ -k 5f15c27d-1f8a-3baa-a136-cd624ba7c9b7 "
+                 //sh "dotnet nuget push /var/lib/jenkins/workspace/Back_main/source/DevOpsProject/DevOpsProject.csproj -s http://192.168.56.115:8081/repository/nuget-hosted/ -k 5f15c27d-1f8a-3baa-a136-cd624ba7c9b7 "
                 //sh 'dotnet nuget push **\\nupkgs\\*.nupkg -s http://192.168.56.115:8081/repository/nuget-hosted/ -k 5f15c27d-1f8a-3baa-a136-cd624ba7c9b7 '
-                    
+                    sh 'docker push 192.168.56.115:8081/test:latest'
                 
          
                     }       }
