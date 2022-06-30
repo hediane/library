87840b7752e274577f86b06b60424b6402e41b19
hediane <49047327+hediane@users.noreply.github.com>
2022-06-30 15:43:25 +0200

    up

--- vars/SonarQubeAngular.groovy
+++ vars/SonarQubeAngular.groovy
@@ -9,7 +9,7 @@ def call (Map config)
                             dir("${config.ProjectName}") {
                                     sh " ls -la ${pwd()}"
                                     //sh "dotnet ${scannerHome}/bin/sonar-scanner  /k:AosoFront /d:sonar.host.url= http://192.168.56.113:9000"
-                                    sh "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=Front -Dsonar.login=eb77d83ff776ed7e511ccb6f98db5163f1af4f9f "
+                                    sh "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=Front -Dsonar.login=580029abd6076347f7f7115089508a365415fef3"
                                     //sh "${scannerHome}/bin/sonar-scanner -D /k:Aoso -D /d:sonar.host.url=http://192.168.56.113:9000  -D /d:sonar.login=ab9f339761ec69b84c33072c739b28b604d3f8ce "
                                     //sh "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=Aosofront "
                                     
