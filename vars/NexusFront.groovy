c62badba2ac453a0be936b546d2972e05d42437b
hediane <49047327+hediane@users.noreply.github.com>
2022-06-30 15:30:22 +0200

    up

--- vars/NexusFront.groovy
+++ vars/NexusFront.groovy
@@ -4,7 +4,7 @@ def call (Map config)
     node
         {
             
-            dir("/var/jenkins_home/workspace/Front_main/source/DevOpsFront") {
+            /*dir("/var/jenkins_home/workspace/Front_main/source/DevOpsFront") {
                         stage('Packing') 
 
                         {
@@ -13,7 +13,7 @@ def call (Map config)
                              sh 'ls -a'
                             sh "ls -a ${config.dist}"
 
-                        }
+                        }*/
             
             stage('Publish') 
                 { 
@@ -25,10 +25,28 @@ def call (Map config)
                    //sh 'npm install --no-package-lock'
                    //sh "npm adduser --registry=http://192.168.56.115:8081/repository/aosoFront/ --always-auth"
                   //sh "npm login --registry=http://192.168.56.115:8081/repository/aosoFront/ publish /var/jenkins_home/workspace/Front_main/source/DevOpsFront/dev-ops-front-0.0.0.tgz"
-                    sh "npm login --registry http://192.168.56.115:8081/repository/aosoFront/"
-                    sh 'ng build'
-                    sh "npm publish /var/jenkins_home/workspace/Front_main/source/DevOpsFront/dev-ops-front-0.0.0.tgz"
-                }
+                    //sh "npm login --registry http://192.168.56.115:8081/repository/aosoFront/"
+                    //sh 'ng build'
+                    //sh "npm publish /var/jenkins_home/workspace/Front_main/source/DevOpsFront/dev-ops-front-0.0.0.tgz"
+               stage('Creating our image'){      
+                        //def version = "latest"
+                        //sh 'docker build -f "${config.Dockerfile}" -t nexus_docker/aoso '
+                        dir("${config.Dockerfile}")
+                            {
+                            dockerImage = docker.build "nexus-udd/aosoFront" + ":latest" 
+                            }
+                    }
+                        stage('push image in nexus'){      
+                                //def version = "latest"
+                                //sh 'docker build -f "${config.Dockerfile}" -t nexus_docker/aoso '
+                                docker.withRegistry( 'http://192.168.56.115:8082/repository/nexus-udd', 'nexus-udd' ) { 
+                                    dockerImage.push() 
+                                } 
+                             }
+             
+        }
+
+               }
         }
         }
         
