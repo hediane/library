1908366d12745c18a269f559d4c8dbb789d14b4a
hediane <49047327+hediane@users.noreply.github.com>
2022-07-05 15:39:26 +0200

    up

--- vars/NexusFront.groovy
+++ vars/NexusFront.groovy
@@ -4,45 +4,29 @@ def call (Map config)
     node
         {
             
-            /*dir("/var/jenkins_home/workspace/Front_main/source/DevOpsFront") {
-                        stage('Packing') 
+            stage('Creating our image'){      
+                    sh "ls -la ${pwd()}"
+                    sh "ls -la ${pwd()}/source"
+                    sh "mkdir -p /srv/aoso/DevOps/frontend "
+                    sh "cp -r ${config.DestinationFolder} ${config.DestinationFile}"
+                    sh "cp -r ${pwd()}${config.DockerfileLocation} ${config.DestinationProject}"
+                    sh "cp -r ${pwd()}${config.DockerfileLocation} ${config.DestinationFile}"
+                    sh "ls -a ${config.DestinationFile}"
+                    sh "cp -r ${pwd()}${config.dockerComposeFileLocation} ${config.DestinationFile}"
+                    sh "cp -r ${pwd()}${config.nginxLocation} ${config.DestinationFile}"
+                    sh "ls -la ${config.DestinationNginx} "
+                    sh "ls -la ${config.DestinationFile} "
 
-                        {
-                            sh "cd ${config.dist}"
-                            sh 'npm pack'
-                             sh 'ls -a'
-                            sh "ls -a ${config.dist}"
-
-                        }*/
-            
-            /*stage('Publish') 
-                { 
-                //sh 'npm login --registry http://192.168.56.115:8081/repository/aosoFront/'
-               // sh 'ng build ng-trc-lib'
-                //sh 'npm publish /var/jenkins_home/workspace/Front_main/source/DevOpsFront/dev-ops-front-0.0.0.tgz'
-                   //sh "npm token create"   
-                       
-                   //sh 'npm install --no-package-lock'
-                   //sh "npm adduser --registry=http://192.168.56.115:8081/repository/aosoFront/ --always-auth"
-                  //sh "npm login --registry=http://192.168.56.115:8081/repository/aosoFront/ publish /var/jenkins_home/workspace/Front_main/source/DevOpsFront/dev-ops-front-0.0.0.tgz"
-                    //sh "npm login --registry http://192.168.56.115:8081/repository/aosoFront/"
-                    //sh 'ng build'
-                    //sh "npm publish /var/jenkins_home/workspace/Front_main/source/DevOpsFront/dev-ops-front-0.0.0.tgz"
-                }*/
-               stage('Creating our image'){      
-                        //def version = "latest"
-                        //sh 'docker build -f "${config.Dockerfile}" -t nexus_docker/aoso '
-                        dir("${config.Dockerfile}")
+                    dir("${config.Dockerfile}")
                             {
                             dockerImage = docker.build "image-front/aoso" + ":latest" 
                             }
                         
                     }
-                        stage('push image in nexus'){      
-                                //def version = "latest"
-                                //sh 'docker build -f "${config.Dockerfile}" -t nexus_docker/aoso '
-                                docker.withRegistry( 'http://149.102.138.184:8082/repository/Aosora', 'NexusSecret' ) { 
-                                    dockerImage.push() 
+            stage('push image in nexus'){      
+                                
+                    docker.withRegistry( 'http://149.102.138.184:8082/repository/Aosora', 'NexusSecret' ) { 
+                        dockerImage.push() 
                                 } 
                              }
              
