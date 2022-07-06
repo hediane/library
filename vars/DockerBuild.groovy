2cea1afa4a89fba89a595b4a3de535758a1a1f17
hediane <49047327+hediane@users.noreply.github.com>
2022-07-06 15:10:24 +0200

    up

--- vars/DockerBuild.groovy
+++ vars/DockerBuild.groovy
@@ -8,27 +8,6 @@ def call (Map config)
                    
                     wrap([$class: 'BuildUser']) {
                     def user = env.BUILD_USER_ID
-                    //echo "${user}"
-                    //def list = ['AmaniGHADDAB']
-                    //echo "${config.devValidator}"
-                
-                   /* if ("${config.devValidator}".contains("${user}"))
-                    {
-                       sh "${config.dockerComposeLocation} -f ${config.dockerComposeDestination} up -d"
-                    }
-                    else
-                    {    
-                        //slackSend color: 'danger', channel: '#devops', message: "<${currentBuild.absoluteUrl}|Server build ${env.BUILD_NUMBER}> failed to deploy build "
-	                       
-                            //input message: 'Do you want to approve the deploy in production?', ok: 'Yes'
-                           if( isApproved = input(id: 'someId',message: 'Approve?', submitter: "${config.devValidator}.contains('${user}')",
-                            parameters: [choice( 
-                                        choices: ['No', 'Yes'],
-                                        description: 'some description',
-                                        name: 'some name')]
-                                ) == 'Yes')
-                                            
-                                }*/
                         
                      if ("${config.devValidator}".contains("${user}"))
                     {     
--- vars/Nexus.groovy
+++ vars/Nexus.groovy
@@ -3,18 +3,7 @@ def call (Map config)
     node
         {
                 stage('CREATING OUR IMAGE'){      
-                         sh "ls -la ${pwd()}"
-                 sh "ls -la ${pwd()}/source"
-                 sh "mkdir -p /srv/aoso/DevOps/backend "
-                 sh "ls -a /srv/aoso/DevOps/backend/"
-                 sh "cp -r ${config.DestinationFolder} ${config.DestinationFile}"
-                 sh "cp -r ${pwd()}${config.DockerfileLocation} ${config.DestinationProject}"
-                 sh "ls -la ${config.DestinationProject} "
-                 sh "cp -r ${pwd()}${config.dockerComposeFileLocation} ${config.DestinationFile}"
-                 sh "cp -r ${pwd()}${config.nginxLocation} ${config.DestinationFile}"
-                 sh "ls -la ${config.DestinationNginx} "
-                 sh "ls -la ${config.DestinationFile} "
-
+                    
                         dir("${config.Dockerfile}")
                             {
                             sh "ls -a"
