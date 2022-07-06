59fea5a4ce44aac6de461aee60510d8a05e0a1e0
hediane <49047327+hediane@users.noreply.github.com>
2022-07-06 13:50:54 +0200

    up

--- vars/TestUnitaire.groovy
+++ vars/TestUnitaire.groovy
@@ -4,7 +4,7 @@ def call (Map config)
         {
     
                 stage('Test unitaire') 
-                {  catchError
+                {  
                  catchError (buildResult: 'SUCCESS', stageResult: 'FAILURE') {
                     sh 'dotnet test --logger:"trx;logFileName=report.xml" source/DevOpsProject/DevOpsProject.sln"'
                 }
