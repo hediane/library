c143a26705c0691a1bb5de0622f7e4577b04484d
hediane <49047327+hediane@users.noreply.github.com>
2022-07-06 12:03:16 +0200

    UP

--- vars/Nexus.groovy
+++ vars/Nexus.groovy
@@ -15,27 +15,25 @@ def call (Map config)
                     //sh 'dotnet build --configuration Release'
                     //sh 'dotnet pack --no-build --output nupkgs'
                 }*/
-                stage('Creating our image'){      
+                stage('CREATING OUR IMAGE'){      
                         //def version = "latest"
                         //sh 'docker build -f "${config.Dockerfile}" -t nexus_docker/aoso '
                         dir("${config.Dockerfile}")
                             {
                             sh "ls -a"
 
-                            dockerImage = docker.build "image-back/aoso" + ":${config.tags}" 
+                            dockerImage = docker.build "image-back/aoso" + ":${config.ImageVersion}" 
 
                             }
                         dir("${config.DestinationNginx}")
                             {
-                            dockerImagenginx = docker.build "image-nginx-backend/aoso" + ":${config.tags}" 
+                            dockerImagenginx = docker.build "image-nginx-backend/aoso" + ":${config.ImageVersion}" 
                             }
                     }
-                        stage('push image in nexus'){      
-                                //def version = "latest"
-                                //sh 'docker build -f "${config.Dockerfile}" -t nexus_docker/aoso '
+                        stage('PUSH IMAGE IN NEXUS'){      
                                 docker.withRegistry( 'http://149.102.138.184:8082/repository/Aosora', 'NexusSecret' ) { 
                                     dockerImage.push() 
-                                     dockerImagenginx.push() 
+                                    dockerImagenginx.push() 
                                 }
                     
                              }
