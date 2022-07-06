770f2e278466e476bb76b87d7855a2394ee0220d
hediane <49047327+hediane@users.noreply.github.com>
2022-07-06 15:42:43 +0200

    up

--- vars/CopyFolderFront.groovy
+++ vars/CopyFolderFront.groovy
@@ -6,13 +6,7 @@ def call (Map config)
             {    
                 sh "ls -la ${pwd()}"
                 sh "ls -la ${pwd()}/source"
-        
-                 //sh "cd /var/jenkins_home/workspace/"
                 sh "mkdir -p /srv/aoso/DevOps/frontend "
-                 //sh "rm -rf /var/jenkins_home/workspace/aoso/DevOps/back"
-                //sh "ls -a ${config.DestinationFolder1}"
-                //sh "ls -a ${config.DestinationFolder2}"
-                //sh "ls -a ${config.DestinationFile}"
                 sh "cp -r ${config.DestinationFolder} ${config.DestinationFile}"
                 sh "cp -r ${pwd()}${config.DockerfileLocation} ${config.DestinationProject}"
                 sh "cp -r ${pwd()}${config.DockerfileLocation} ${config.DestinationFile}"
