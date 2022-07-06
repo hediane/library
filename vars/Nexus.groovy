4314c79edf94cb49857260eba5f50d632d93f438
hediane <49047327+hediane@users.noreply.github.com>
2022-07-06 12:08:12 +0200

    up

--- vars/Nexus.groovy
+++ vars/Nexus.groovy
@@ -2,19 +2,6 @@ def call (Map config)
 {  
     node
         {
-            /* {   stage('Packing') 
-                {  
-                   
-                //sh "${config.dockerComposeLocation} -f ${config.dockerComposeNexusDestination} up -d"
-                   sh "dotnet clean"
-                   sh "dotnet build"
-                   sh "dotnet pack"
-                   //sh "ls -a /var/lib/jenkins/workspace/Back_main/source/DevOpsProject/bin/Debug/"
-                   //sh "ls -a /var/lib/jenkins/workspace/Back_main/source/DevOpsProject/bin/Debug/aosora.1.0.0.nupkg"
-                    //sh 'dotnet clean'
-                    //sh 'dotnet build --configuration Release'
-                    //sh 'dotnet pack --no-build --output nupkgs'
-                }*/
                 stage('CREATING OUR IMAGE'){      
                          sh "ls -la ${pwd()}"
                  sh "ls -la ${pwd()}/source"
@@ -47,19 +34,7 @@ def call (Map config)
                              }
              
         }
-           /* stage('Publish') 
-                { 
-                 //sh "dotnet nuget push /var/lib/jenkins/workspace/Back_main/source/DevOpsProject/DevOpsProject.csproj -s http://192.168.56.115:8081/repository/nuget-hosted/ -k 5f15c27d-1f8a-3baa-a136-cd624ba7c9b7 "
-                //sh 'dotnet nuget push **\\nupkgs\\*.nupkg -s http://192.168.56.115:8081/repository/nuget-hosted/ -k 5f15c27d-1f8a-3baa-a136-cd624ba7c9b7 '
-                   // sh 'docker build -t backend_backend .'
-                    sh 'docker login http://192.168.56.115:8082/repository/nexus_docker/"'
-                    sh 'docker tag backend_backend http://192.168.56.115:8082/repository/nexus_docker/backend_backend:latest"'
-                    sh "docker push http://192.168.56.115:8082/repository/nexus_docker/backend_backend:latest'"
-                
-                  
-         
-                    }   */   
-     
+          
         
         
 }
