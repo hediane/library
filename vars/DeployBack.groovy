8c7a25b0b95050aeff026cbbc964a822b05665a1
hediane <49047327+hediane@users.noreply.github.com>
2022-07-07 14:02:43 +0200

    up

--- vars/DeployBack.groovy
+++ vars/DeployBack.groovy
@@ -16,12 +16,12 @@ def call (Map config)
                             {   
 
                                //sh "${config.dockerComposeLocation} -f ${config.dockerComposeDestination} up -d"
-                                sh "ssh ${DevServer} 'docker pull ${ImagePull}'"
-                                sh "ssh ${DevServer} 'docker pull ${ImageNginxPull}'"
-                                sh "ssh ${DevServer} 'mkdir -p ${DestinationFolder}'"
-                                sh "scp ${dockerComposeDestination} ${DevServer}:${dockerComposeDestination}"
-                                sh "ssh  ${DevServer} docker-compose -f ${dockerComposeDestination} up -d"        
-                                input message: "YOU CAN CHECK LOGS USING THIS LINKS ${dockerComposeDestination} "
+                                sh "ssh ${config.DevServer} 'docker pull ${config.ImagePull}'"
+                                sh "ssh ${config.DevServer} 'docker pull ${config.ImageNginxPull}'"
+                                sh "ssh ${config.DevServer} 'mkdir -p ${config.DestinationFolder}'"
+                                sh "scp ${config.dockerComposeDestination} ${config.DevServer}:${config.dockerComposeDestination}"
+                                sh "ssh  ${config.DevServer} docker-compose -f ${config.dockerComposeDestination} up -d"        
+                                input message: "YOU CAN CHECK LOGS USING THIS LINKS ${config.dockerComposeDestination} "
                             }
                         else 
                         {
