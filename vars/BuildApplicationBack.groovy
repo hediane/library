4c979f30a0b8d42a92e53b212141c3a71d07bc1e
hediane <49047327+hediane@users.noreply.github.com>
2022-07-07 13:50:12 +0200

    up

--- vars/BuildApplicationBack.groovy
+++ vars/BuildApplicationBack.groovy
@@ -4,14 +4,7 @@ def call (Map config) {
         {
         stage ('BUILD PROJECT ')
             {
-            sh "mkdir -p ${config.DestinationFolder} "
-            sh "cp -r ${config.source} ${config.DestinationFolder}"
-            sh "cp -r ${pwd()}${config.DockerfileLocation} ${config.DestinationProject}"
-            sh "cp -r ${pwd()}${config.dockerComposeFileLocation} ${config.DestinationFolder}"
-            sh "cp -r ${pwd()}${config.nginxLocation} ${config.DestinationFolder}"
-            sh "cp -r ${config.nunit} ${config.DestinationFolder}"
-            sh "cp -r ${config.projectTest} ${config.DestinationFolder}"
-
+           
             dir("${config.DestinationProject}")
                {
                 sh'dotnet restore'
--- vars/NexusBack.groovy
+++ vars/NexusBack.groovy
@@ -1,28 +1,31 @@
-def call (Map config)
-{  
+def call (Map config) {
     node
         {
-                stage('CREATING OUR IMAGE'){      
-                    
-                        dir("${config.DestinationProject}")
+        stage('CREATING OUR IMAGE') {
+            sh "mkdir -p ${config.DestinationFolder} "
+            sh "cp -r ${config.source} ${config.DestinationFolder}"
+            sh "cp -r ${pwd()}${config.DockerfileLocation} ${config.DestinationProject}"
+            sh "cp -r ${pwd()}${config.dockerComposeFileLocation} ${config.DestinationFolder}"
+            sh "cp -r ${pwd()}${config.nginxLocation} ${config.DestinationFolder}"
+            sh "cp -r ${config.nunit} ${config.DestinationFolder}"
+            sh "cp -r ${config.projectTest} ${config.DestinationFolder}"
+            dir("${config.DestinationProject}")
                             {
-                            dockerImage = docker.build "${config.ImageBuild}" + ":${config.ImageVersion}" 
+                dockerImage = docker.build "${config.ImageBuild}" + ":${config.ImageVersion}"
                             }
-                        dir("${config.DestinationNginx}")
+            dir("${config.DestinationNginx}")
                             {
-                            dockerImagenginx = docker.build "${config.ImagenginxBuild}"+ ":${config.ImageVersion}" 
+                dockerImagenginx = docker.build "${config.ImagenginxBuild}" + ":${config.ImageVersion}"
                             }
-                    }
-                        stage('PUSH IMAGE IN NEXUS'){      
-                                docker.withRegistry( "${config.nexusRepository}", "${config.credentiel_id}" ) { 
-                                    dockerImage.push() 
-                                    dockerImagenginx.push() 
-                                }
-                    
-                             }
-             
+                }
+
+
+                
+        stage('PUSH IMAGE IN NEXUS') {
+            docker.withRegistry( "${config.nexusRepository}", "${config.credentiel_id}" ) {
+                dockerImage.push()
+                dockerImagenginx.push()
+            }
         }
-          
-        
-        
+        }
 }
