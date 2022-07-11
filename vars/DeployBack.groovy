ee35daa461a9105694ba5938125bc56bf1f9e1b3
hediane <49047327+hediane@users.noreply.github.com>
2022-07-11 17:03:16 +0200

    up

--- vars/DeployBack.groovy
+++ vars/DeployBack.groovy
@@ -20,7 +20,7 @@ def call (Map config)
                                 //sh "ssh ${config.DevServer} 'docker pull ${config.ImageNginxPull}'"
                                 sh "ansible -m ping  dev"
                                 sh "ssh ${config.DevServer} 'mkdir -p ${config.DestinationFolder}'"
-                                sh "ansible -playbook ansible/playbook.yml -i inventory/dev.ini  "
+                                sh "ansible --playbook-dir ansible/playbook.yml -i inventory/dev.ini  "
                                 //sh "scp ${config.dockerComposeDestination} ${config.DevServer}:${config.dockerComposeDestination}"
                                 //sh "ssh  ${config.DevServer} docker-compose -f ${config.dockerComposeDestination} up -d"        
                                 input message: "YOU CAN CHECK LOGS USING THIS LINKS ${config.url_Elasticsearch_Kibana} "
