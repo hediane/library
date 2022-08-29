f9a6728fb0546c98b8511af5fa27685135ce5fdf
hediane <49047327+hediane@users.noreply.github.com>
2022-08-29 03:38:06 +0200

    up

--- vars/DeployFront.groovy
+++ vars/DeployFront.groovy
@@ -8,10 +8,13 @@ def call (Map config) {
                 if ("${config.devValidator}".contains("${user}")) {
                     if (input (id: 'someId', message: 'Do you want to approve the deploy in production?',
                             parameters: [choice( choices: ['No', 'Yes'], description: 'DO YOU CONFIRME', name: 'some name')]) == 'Yes') {
-                        sh "ssh ${config.DevServer} 'docker pull ${config.ImagePull}'"
-                        sh "ssh ${config.DevServer} 'mkdir -p ${config.DestinationFolder}'"
-                        sh "scp  ${config.dockerComposeDestination} ${config.DevServer}:${config.DestinationFolder}"
-                        sh "ssh ${config.DevServer} docker-compose -f ${config.dockerComposeDestination} up -d"
+                        sh "ansible -m ping  front"
+                        sh "ansible-playbook ${config.ansibleplaybook} -i ${config.ansibleinventory}"
+                        //sh "ssh ${config.DevServer} 'docker pull ${config.ImagePull}'"
+                        //sh "ssh ${config.DevServer} 'mkdir -p ${config.DestinationFolder}'"
+                        //sh "scp  ${config.dockerComposeDestination} ${config.DevServer}:${config.DestinationFolder}"
+                        //sh "ssh ${config.DevServer} docker-compose -f ${config.dockerComposeDestination} up -d"
+
                         }else{
                         input message: 'ABORD'
                         }
